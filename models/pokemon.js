063e6a6d6097a7dd74c93b32484a27afa420f501
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-06-01 12:05:11 -0500

    started a seed and schema

---
+++ models/pokemon.js
@@ -0,0 +1,30 @@
+/**
+ * @file pokemon.js
+ * @description pokemon schema for mongoDB
+ */
+
+'use strict';
+const mongoose = require('mongoose');
+
+const pokemonSchema = new mongoose.Schema({
+    	
+abilities: Array,
+base_experience: Number,
+forms: Array,
+game_indices: Array,
+height: Number,
+held_items:	Array,
+id: Number,
+is_default:	Boolean,
+location_area_encounters:	String,
+moves: Array,
+name:	String,
+order: Number,
+past_types:	Array,
+species: Object,
+sprites, Object,
+stats: Array,
+types: Array,
+weight:	Number,
+});
+
