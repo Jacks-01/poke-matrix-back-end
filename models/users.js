dc46cb330dd6bb856778cf646f0d9bd9133f8fed
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-06-02 15:51:12 -0500

    created a user schema

---
+++ models/users.js
@@ -0,0 +1,18 @@
+/**
+ * @file users.js
+ * @description a schema for a user profile
+ */
+'use strict';
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    favorites: Object
+})
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
